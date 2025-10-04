6eb42ab85cfc1afdcc2d26bf7bb8cead62c87e49
Sahil Khan <sahilsmiling@gmail.com>
2025-10-04 19:36:00 +0530

    first commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.law_chatbot"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
